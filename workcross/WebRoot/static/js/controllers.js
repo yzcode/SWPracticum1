86194ab7a27be67eb5c7b7f6b1c333fc6f930844
yzcode <codeforyz@gmail.com>
2014-12-05 21:52:59 +0800

    dashboard

--- workcross/WebRoot/static/js/controllers.js
+++ workcross/WebRoot/static/js/controllers.js
@@ -9,6 +9,17 @@ workxControllers.controller('userMain', ['$scope', '$http',
         $http.get('../static/json/testjson/user.json').success(
             function(data){
                 $scope.user= data;
+                $scope.nowpage = 'dashboard';
+            }
+        )
+    }]);
+
+workxControllers.controller('projects', ['$scope', '$http','userMain',
+    function($scope, $http,userMain) {
+        $http.get(settings.webroot+'static/json/testjson/project.json').success(
+            function(data){
+                userMain.nowpage = 'projects';
+                $scope.projects= data;
             }
         )
     }]);
---
+++ workcross/WebRoot/static/js/settings.js
@@ -0,0 +1,9 @@
+/**
+ * Created by yangz on 2014/12/5.
+ */
+
+var settings = new Object();
+
+settings.j2ee = false;
+if(!settings.j2ee) settings.webroot = '/SWPracticum1/workcross/WebRoot/';
+else settings.webroot = '/workcross/WebRoot/';
