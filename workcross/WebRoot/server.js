9d55b4eb00978f17ca3e48b66e39ebd93c238452
yzcode <codeforyz@gmail.com>
2014-12-12 18:49:41 +0800

    task finish

--- workcross/WebRoot/server.js
+++ workcross/WebRoot/server.js
@@ -9,6 +9,12 @@ app.get('/workcross/dashboard', function(req, res){
 app.get('/workcross/', function(req, res){
     res.sendfile(__dirname+'/index.html');
 });
+app.post('/workcross/api/tasks/', function(req, res){
+    res.sendfile(__dirname+'/static/json/testjson/new_task.json');
+});
+app.post('/workcross/api/entries/', function(req, res){
+    res.sendfile(__dirname+'/static/json/testjson/new_entry.json');
+});
 var server = app.listen(8083, function() {
     console.log('Listening on port %d', server.address().port);
 });
